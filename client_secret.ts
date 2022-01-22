018106b75019e4a70a67fd1ff9fc7045b2f3a1ee
vladtinishov <54472129+vladtinishov@users.noreply.github.com>
2022-01-22 15:13:30 +0600

    Update client_secret.ts

--- client_secret.ts
+++ client_secret.ts
@@ -1,12 +1,12 @@
 export const clientSecret = {
   web: {
     client_id:
-      '809079846293-pbfaact5bqs5p7o3bvuh8tmo5bvdsuik.apps.googleusercontent.com',
-    project_id: 'bubbly-enigma-318210',
-    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-    token_uri: 'https://oauth2.googleapis.com/token',
-    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-    client_secret: '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',
-    redirect_uris: ['http://localhost:3000/google/redirect'],
+      '',
+    project_id: ' ',
+    auth_uri: ' ',
+    token_uri: ' ',
+    auth_provider_x509_cert_url: ' ',
+    client_secret: ' ',
+    redirect_uris: [' '],
   },
 };
